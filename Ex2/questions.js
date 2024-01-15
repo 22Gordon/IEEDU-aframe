8ea5df4029694ac11bbcda4221a50c48a68ffc05
Gonçalo Quesado <goncaloquesado@gmail.com>
2024-01-15 14:59:57 +0000

    Commit 15 - Quizz

---
+++ Ex2/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        question: "Who painted the Mona Lisa?",
+        image: "img/monalisa.avif",
+        answer: "Leonardo da Vinci",
+        options: [
+            "Vincent van Gogh",
+            "Leonardo da Vinci",
+            "Pablo Picasso",
+            "Michelangelo"
+        ]
+    },
+    {
+        question: "What is the title of the famous painting with swirling stars by Vincent van Gogh?",
+        image: "img/StarryNight.jpg",
+        answer: "Starry Night",
+        options: [
+            "The Last Supper",
+            "Guernica",
+            "Starry Night",
+            "The Persistence of Memory"
+        ]
+    },
+    {
+        question: "Who created 'The Great Wave off Kanagawa'?",
+        image: "img/wave.jpg",
+        answer: "Katsushika Hokusai",
+        options: [
+            "Vincent van Gogh",
+            "Claude Monet",
+            "Katsushika Hokusai",
+            "Pablo Picasso"
+        ]
+    },
+    {
+        question: "What is the title of the sculpture depicting Napoleon Bonaparte by Auguste Rodin?",
+        image: "img/napoleao.jpg",
+        answer: "The Busto of Napoleon Bonaparte",
+        options: [
+            "The Thinker",
+            "The Busto of Napoleon Bonaparte",
+            "David",
+            "Venus de Milo"
+        ]
+    },
+    {
+        question: "Who painted 'Woman with a Parasol'?",
+        image: "img/women.jpg",
+        answer: "Claude Monet",
+        options: [
+            "Pierre-Auguste Renoir",
+            "Edgar Degas",
+            "Claude Monet",
+            "Camille Pissarro"
+        ]
+    }
+];
